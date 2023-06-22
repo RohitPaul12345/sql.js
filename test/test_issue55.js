1c40461db507364f612b199db1d658e0876708e4
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-06-23 00:48:02 +0530

    Update test_issue55.js

--- test/test_issue55.js
+++ test/test_issue55.js
@@ -1,21 +1,21 @@
 exports.test = function(SQL, assert) {
-  var fs = require('fs');
-  var path = require('path');
+  let fs = require('fs');
+  let path = require('path');
 
-  var filebuffer = fs.readFileSync(path.join(__dirname, 'issue55.db'));
+  let filebuffer = fs.readFileSync(path.join(__dirname, 'issue55.db'));
 
   //Works
-  var db = new SQL.Database(filebuffer);
+  let db = new SQL.Database(filebuffer);
 
-  var origCount = db.prepare("SELECT COUNT(*) AS count FROM networklocation").getAsObject({}).count;
+  let origCount = db.prepare("SELECT COUNT(*) AS count FROM networklocation").getAsObject({}).count;
 
   db.run("INSERT INTO networklocation (x, y, network_id, floor_id) VALUES (?, ?, ?, ?)", [123, 123, 1, 1]);
 
-  var count = db.prepare("SELECT COUNT(*) AS count FROM networklocation").getAsObject({}).count;
+  let count = db.prepare("SELECT COUNT(*) AS count FROM networklocation").getAsObject({}).count;
 
   assert.equal(count, origCount + 1, "The row has been inserted");
-  var dbCopy = new SQL.Database(db.export());
-  var newCount = dbCopy.prepare("SELECT COUNT(*) AS count FROM networklocation").getAsObject({}).count;
+  let dbCopy = new SQL.Database(db.export());
+  let newCount = dbCopy.prepare("SELECT COUNT(*) AS count FROM networklocation").getAsObject({}).count;
   assert.equal(newCount, count, "export and reimport copies all the data");
 };
 
